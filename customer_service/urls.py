fbf73aa509063eb15aa76e13ec62e86885942f42
siddhi0409 <89137190+siddhi0409@users.noreply.github.com>
2024-06-02 22:47:57 +0530

    first commit

---
+++ customer_service/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'), 
+    path('register/', views.register, name='register'),
+    path('submit_request/', views.submit_request, name='submit_request'),
+    path('track_requests/', views.track_requests, name='track_requests'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+   
+]
